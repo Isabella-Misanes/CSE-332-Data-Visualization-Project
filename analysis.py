4ac9ee1e68dedd3f0699614f86acc22151662850
Isabella-Misanes <99161680+Isabella-Misanes@users.noreply.github.com>
2023-12-06 17:04:49 -0500

    Remove extra files

--- analysis.py
+++ analysis.py
@@ -7,24 +7,6 @@ from sklearn.metrics import pairwise_distances
 from sklearn.manifold import MDS
 from sklearn.cluster import KMeans
 
-def getProjections():
-    data = pd.read_csv('Lab 1 Spreadsheet - 2011.csv', usecols=[2,3,4,5,6,7,8,9])
-
-    attributes = data[["Number of People Migrated from New York", "Population with Different State of Residence 1 Year Ago", "Median Age Living in State (Years)", "Population 65+ Years Old (%)", "25+ Years Old Population with Bachelor's Degree or Higher (%)", "No Healthcare Coverage (%)", "Households with One or More People Under 18 Years (%)", "Civilian Employment Rate (%)"]]
-
-    scaler = StandardScaler()
-    scaled_data = scaler.fit_transform(attributes)
-
-    pca = PCA(n_components=2)
-    principal_components = pca.fit_transform(scaled_data)
-
-    loadings = pca.components_[:2, :]
-    print(loadings)
-
-    #plotted points
-    projections_df = pd.DataFrame(data=principal_components, columns=["PC1", "PC2"])
-    projections_df.to_csv('points.csv')
-
 def findEuclidean():
     data = pd.read_csv('Lab 1 Spreadsheet - 2011.csv', usecols=[2,3,4,5,6,7,8,9])
 
@@ -52,12 +34,6 @@ def elbowMethod():
         kmeans.fit(features)
         inertias.append(kmeans.inertia_)
 
-    plt.plot(k_values, inertias, marker='o')
-    plt.title('Elbow Method for Optimal K')
-    plt.xlabel('Number of Clusters (K)')
-    plt.ylabel('Sum of Squared Distances (Inertia)')
-    plt.show()
-
     # It can be determined from the resulting plot that the elbow point is at 3
 
     optimal_k =  3
@@ -67,6 +43,6 @@ def elbowMethod():
 
     
 if __name__ == "__main__":
-    # getProjections()
-    # findEuclidean()
-    elbowMethod()
+    #findEuclidean()
+    #elbowMethod()
+    pass
